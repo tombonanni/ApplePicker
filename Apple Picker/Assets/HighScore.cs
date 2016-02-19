605055d0e764fe761ae6e1cb6d9c10e63c7aff88
unknown <TRB82@CILABS-119A-09.scilsnet.rutgers.edu>
2016-02-18 20:40:49 -0500

    adding everything

---
+++ Apple Picker/Assets/HighScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 1000;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("AplePickerHighscore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+
+	void Update () {
+
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        if(score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+
+	}
+}
